079be21e7491d37c0038a5a5704188cb8b1bb182
Ashwin Srinath <atrikut@clemson.edu>
2017-02-28 10:07:44 -0500

    Install gcc 5.0 for caffe

--- caffe/install_caffe_deps.sh
+++ caffe/install_caffe_deps.sh
@@ -1,5 +1,9 @@
+apt-get install -y --force-yes software-properties-common python-software-properties
+apt-add-repository -y ppa:ubuntu-toolchain-r/test
+apt-get update
+apt-get install -y --force-yes gcc-5 g++-5
 apt-get install -y --force-yes git
-apt-get install -y --force-yes libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
+apt-get install -y --force-yes libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler libopenblas-dev
 apt-get install -y --force-yes --no-install-recommends libboost-all-dev
 apt-get install -y --force-yes libgflags-dev libgoogle-glog-dev liblmdb-dev
 apt-get install -y --force-yes libatlas-base-dev
