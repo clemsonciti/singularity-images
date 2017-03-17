4262d225d5287d36a2a4f49955efa6460f19d389
Ashwin Srinath <atrikut@clemson.edu>
2017-03-17 13:32:16 -0400

    fix renaming caffe executables and add digits setup script

--- caffe/install_caffe.sh
+++ caffe/install_caffe.sh
@@ -5,7 +5,13 @@ make all -j8
 make pycaffe
 make distribute
 mv distribute /opt/caffe
+for file in /opt/caffe/bin/*
+do
+    mv $file /opt/caffe/bin/`basename $file .bin`
+done
 echo "export PYTHONPATH=/opt/caffe/python:\$PYTHONPATH" >> /environment
 echo "export PATH=/opt/caffe/bin:\$PATH" >> /environment
 echo "export LD_LIBRARY_PATH=/opt/caffe/lib:\$LD_LIBRARY_PATH" >> /environment
 echo "export LIBRARY_PATH=/opt/caffe/lib:\$LIBRARY_PATH" >> /environment
+
+rm -rf ~/caffe
---
+++ caffe/install_digits.sh
@@ -0,0 +1,7 @@
+apt-get install -y --force-yes --no-install-recommends graphviz python-dev python-flask python-flaskext.wtf python-gevent python-pyparsing
+DIGITS_ROOT=/opt/DIGITS
+git clone https://github.com/NVIDIA/DIGITS.git $DIGITS_ROOT
+pip install setuptools==33.1.1 # needed to avoid pyparsing errors..
+pip install -r $DIGITS_ROOT/requirements.txt
+pip install -e $DIGITS_ROOT
+echo "export DIGITS_JOBS_DIR=/home/\$USER/.digits_jobs" >> /environment
