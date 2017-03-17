d77b42c55830b24d0bb474b113371ac9c8425acc
Ashwin Srinath <atrikut@clemson.edu>
2017-03-17 14:06:00 -0400

    Install digits and make scratch dirs in caffe container

--- caffe/build_container.sh
+++ caffe/build_container.sh
@@ -5,3 +5,7 @@ sudo singularity bootstrap ubuntu_caffe_gpu.img ubuntu.def
 sudo singularity exec -B `pwd`:/mnt -w ubuntu_caffe_gpu.img sh /mnt/install_cuda.sh
 sudo singularity exec -B `pwd`:/mnt -w ubuntu_caffe_gpu.img sh /mnt/install_caffe_deps.sh
 sudo singularity exec -B `pwd`:/mnt -w ubuntu_caffe_gpu.img sh /mnt/install_caffe.sh
+sudo singularity exec -B `pwd`:/mnt -w ubuntu_caffe_gpu.img sh /mnt/install_digits.sh
+sudo singularity exec -B `pwd`:/mnt -w ubuntu_caffe_gpu.img mkdir /local_scratch
+sudo singularity exec -B `pwd`:/mnt -w ubuntu_caffe_gpu.img mkdir /scratch2
+sudo singularity exec -B `pwd`:/mnt -w ubuntu_caffe_gpu.img mkdir /software
