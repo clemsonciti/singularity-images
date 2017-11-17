b2c5b1c6f5b566df2abe5df961e720fc5cfc0b22
Linh B Ngo <lngo@clemson.edu>
2017-11-17 11:46:28 -0500

    specifies working numpy and scipy

--- circuitscape/install_circuitscape.sh
+++ circuitscape/install_circuitscape.sh
@@ -4,9 +4,9 @@ sudo apt-get install -y python python-dev python-pip
 
 sudo apt-get install -y libblas-dev liblapack-dev libatlas-base-dev gfortran
 
-sudo pip install numpy
-sudo pip install scipy
-sudo pip install pyamg
+sudo pip install numpy==1.12.1
+sudo pip install scipy==0.19.0
+sudo pip install pyamg==3.2.1
 sudo pip install psutil
 sudo apt-get install -y python-wxgtk3.0
 sudo apt-get install -y python-pythoncard
